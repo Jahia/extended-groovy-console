d125419954ed4ae5e465eff72fd18608c06963ed
dsaulnier <dsaulnier@jahia.com>
2021-05-07 16:13:47 +0200

    Added a code snippet to disable the eventlisteners

---
+++ src/main/resources/tools/groovySnippets/disableEventListeners.groovy
@@ -0,0 +1,7 @@
+import org.jahia.services.content.JCRObservationManager
+
+JCRObservationManager.setAllEventListenersDisabled(true)
+
+// write your code here
+
+JCRObservationManager.setAllEventListenersDisabled(false)
